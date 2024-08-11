56992f1cfca6d903e6f3aae7b66279e71e54accb
Joe Ksiazek <hyzer.jak@gmail.com>
2024-08-10 22:09:24 -0400

    Init commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Head } from "@inertiajs/react";
+
+export default function Welcome() {
+    return (
+        <>
+            <Head title="Welcome" />
+            <h1>Welcome to your new Laravel/React app with Inertia!</h1>
+        </>
+    );
+}
